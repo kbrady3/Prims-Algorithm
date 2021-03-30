47323582b81f07afccfc4b67e3c8cffb116bf322
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-03-30 18:11:14 -0500

    Re-initialization

---
+++ PrimsAlgorithm/PrimsAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimsAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
